d4ca0481d94320463dd16500d8bc632134c0a244
Sejal Mahadik <sejalmahadik1519@gmail.com>
2025-07-01 19:02:38 +0530

    some css added

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ const CIRCUMFERENCE = 2 * Math.PI * RADIUS;
 circle.style.strokeDasharray = `${CIRCUMFERENCE} ${CIRCUMFERENCE}`;
 circle.style.strokeDashoffset = `${CIRCUMFERENCE}`;
 
+function animateFlip(elem, newValue) {
+    const span = elem.querySelector("span");
+    if (span.textContent !== newValue) {
+        span.textContent = newValue;
+        elem.classList.remove("animate");
+        void elem.offsetWidth;
+        elem.classList.add("animate");
+    }
+}
+
 function updateClock() {
     const now = new Date();
     let hours = now.getHours();
@@ -20,9 +30,10 @@ function updateClock() {
     const seconds = now.getSeconds();
 
     const ampm = hours >= 12 ? "PM" : "AM";
-    hours = hours % 12 || 12; // Convert 0 -> 12 for midnight
+    hours = hours % 12 || 12;
     hours = hours.toString().padStart(2, "0");
-
+    animateFlip(document.querySelector(".flip:nth-child(1)"), hours);
+    animateFlip(document.querySelector(".flip:nth-child(3)"), minutes);
     hoursElem.textContent = hours;
     minutesElem.textContent = minutes;
     ampmElem.textContent = ampm;
@@ -44,3 +55,19 @@ function toggleTheme() {
 
 setInterval(updateClock, 1000);
 updateClock();
+
+const audio = document.getElementById("background-audio");
+const musicBtn = document.getElementById("music-btn");
+let isPlaying = false;
+
+function toggleMusic() {
+    if (isPlaying) {
+        audio.pause();
+        musicBtn.style.opacity = "0.4"; 
+    } else {
+        audio.play();
+        musicBtn.style.opacity = "1";
+    }
+    isPlaying = !isPlaying;
+}
+
